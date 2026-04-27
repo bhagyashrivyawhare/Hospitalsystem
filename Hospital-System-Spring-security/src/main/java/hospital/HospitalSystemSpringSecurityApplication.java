386b0275f7d7fc3f565a000727f9ff3c8802b83f
bhagyashrivyawhare <vyawharebhagyshree9@gmail.com>
2026-04-27 13:52:58 +0530

    Initial commit

---
+++ Hospital-System-Spring-security/src/main/java/hospital/HospitalSystemSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSystemSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSystemSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ Hospital-System-Spring-security/src/test/java/hospital/HospitalSystemSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSystemSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
